3399bbcd03b56209f630938d4eeefef7b9902722
Joel Lara <joel.lq@hotmail.com>
2016-10-14 17:07:51 -0500

    Refactor index and login

--- index.php
+++ index.php
@@ -1,6 +1,45 @@
 <?php 
-    include_once 'db_config.php';
-?>
+    require_once 'db_config.php';
+    if(!empty($_POST)){
+        $email = $_POST['email'];
+        $firstName = $_POST['firstName'];
+        $lastName = $_POST['lastName'];
+        $originalDate = $_POST['birthdate'];
+        $matricula = $_POST['matricula'];
+        $birthDate = date("Y/m/d", strtotime($originalDate));
+        $sql = "SELECT * FROM student where email = '".$email."'";
+        $result = $mysqli->query($sql);
+        if($result->num_rows === 0){
+            $valid = true;
+            $error = "";
+            if (empty($firstName)) {
+                $error .= "Please enter First Name<br>";
+                $valid = false;
+            }
+            if (empty($lastName)) {
+                $error .= "Please enter Last Name<br>";
+                $valid = false;
+            }
+            if (empty($matricula)) {
+                $error .= "Please enter a Student ID<br>";
+                $valid = false;
+            }
+            if (empty($birthdate)) {
+                $error .= "Please enter your Birthdate<br>";
+                $valid = false;
+            }
+            if($valid){
+                $sqlIns = "INSERT INTO student (`studentID`, `studentName`, `email`, `birthDate`) VALUES ('".$matricula."', '".$firstName." ".$lastName."', '".$email."','".$birthDate."')";
+                $result = $mysqli->query($sqlIns);
+                header("Location: todos.php?matricula=$matricula");
+            }
+        }else{
+            $student = $result->fetch_assoc();
+            $matricula = $student['studentID'];
+            header("Location: todos.php?matricula=$matricula");
+        }
+    }
+ ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -46,7 +85,8 @@
             <h1>Dr. Vallejo</h1>
         </div>
     </div>
-    <form method="post" action="login.php">
+    <form method="post" action="index.php">
+            <p style="color:red"> <?php if(!empty($error))echo $error; ?> </p>
             <div class="row">
                 <div class="six columns">
                     <label for="firstName">Your first name</label>
@@ -57,10 +97,18 @@
                     <input class="u-full-width" name="lastName" type="text" placeholder="El Toro" id="lastName">
                 </div>
             </div>
-            <label for="birthdate">Your birthdate</label>
-            <input class="u-full-width" type="date" name="birthdate" id="birthdate">
+            <div class="row">
+                <div class="six columns">
+                    <label for="matricula">Your student ID</label>
+                    <input class="u-full-width" name="matricula" type="text" placeholder="A0137469" id="matricula">
+                </div>
+                <div class="six columns">
+                    <label for="birthdate">Your birthdate</label>
+                    <input class="u-full-width" type="date" name="birthdate" id="birthdate">
+                </div>
+            </div>
             <label for="email">Your email</label>
-            <input class="u-full-width" type="email" name="email" placeholder="hola@email.com" id="email">
+            <input class="u-full-width" type="email" name="email" placeholder="hola@email.com" id="email" value="<?php if(!empty($email))echo $email; ?>"required>
             <input class="button-primary" type="submit" value="Submit">
     </form>
   </div>
