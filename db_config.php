d66ac91ef0b33e2f7659a473d9009c49cfbbf611
Joel Lara <joel.lq@hotmail.com>
2016-10-14 15:56:47 -0500

    Initial commit

---
+++ db_config.php
@@ -0,0 +1,7 @@
+<?php
+    $host = "localhost:3307";
+    $user = "root";
+    $password = "Jolaqu950804.";
+    $datbase = "mastertodo";
+    $mysqli = new mysqli($host, $user, $password, $datbase);
+?>
---
+++ index.php
@@ -0,0 +1,62 @@
+<?php 
+    include_once 'db_config.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+  <!-- Basic Page Needs
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta charset="utf-8">
+  <title>Basic CRUD for Advanced Database Systems</title>
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <!-- Mobile Specific Metas
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <!-- FONT
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
+
+  <!-- CSS
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/skeleton.css">
+
+  <!-- Favicon
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <link rel="icon" type="image/png" href="images/favicon.png">
+
+</head>
+<body>
+
+  <!-- Primary Page Layout
+  –––––––––––––––––––––––––––––––––––––––––––––––––– -->
+  <div class="container">
+    <div class="row">
+        <div class="eleven column" style="margin-top: 5%">
+            <h1>Database System Project by</h1>  
+            <h3>Joel Lara,Esteban Gil, Daniel Sada</h3>
+        </div>  
+    </div>
+    <div class="row">
+        <div class="eleven column">
+            <h1>Dr. Vallejo</h1>
+        </div>
+    </div>
+    <div class="row">
+      <div class="one column">
+        <?php
+            $sql_query="SELECT * FROM student";
+            $result = $mysqli->query($sql_query);
+            while ($row = $result->fetch_assoc()) {
+                echo '<p>'. $row['studentName'].'</p>';
+            }
+        ?>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
